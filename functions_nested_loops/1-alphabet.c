0d65c8050b596b1626368f2a87cfa8a5d8c1d3d3
Sandya1988 <Aoun.sandy@gmail.com>
2024-02-26 23:32:43 +0200

    dfg

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+int main (void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
