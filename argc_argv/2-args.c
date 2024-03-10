eaa1f9454c6cbb6e15185c65ef2c36c7d64e451d
Sandya1988 <Aoun.sandy@gmail.com>
2024-03-10 23:48:01 +0200

    CC

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
- 
-int main(int argc, char *argv[])
+/**
+ * main -  print name
+ *@argc: int.
+ *@argv: char.
+ * Return: Always 0.
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	int i;
-
-	for (i = 0 ; i < argc ; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
+
