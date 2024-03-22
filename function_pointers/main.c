412070cccc129f6f1ce80252889c297e83c3645b
Sandya1988 <Aoun.sandy@gmail.com>
2024-03-22 22:24:01 +0200

    ssd

--- function_pointers/main.c
+++ function_pointers/main.c
@@ -1,17 +1,31 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-
-/*
- * File: function_pointers.h
- *
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0E-function_pointers directory.
- */
-
+#include "3-calc.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+/**
+ * main - program to perform simple operations
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: exits 0 normally, 98 on argc error, 99 on illegal operator
+ */
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
 
-#endif
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	func = get_op_func(argv[2]);
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
