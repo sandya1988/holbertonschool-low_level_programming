e278f172e1f9decabcb743a1d3113676ff3af0ad
Sandya1988 <Aoun.sandy@gmail.com>
2024-03-27 22:50:15 +0200

    iop

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,3 +1,5 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -5,4 +7,5 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+#endif
 
